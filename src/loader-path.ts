6fb01b4a8b04007a13c381aa2f4a78f1f5e32360
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:10 -0400

    ## 6.3.0
    ### Minor Changes
    
    - fix tsc build directory issues

--- src/loader-path.ts
+++ src/loader-path.ts
@@ -1,9 +1,9 @@
 import path from 'path'
 import fs from 'fs'
-import valid__url from 'valid-url'
+import url_valid from 'valid-url'
 const { access } = fs
 const { R_OK } = fs.constants
-async function _path__file(in_file_path:string) {
+async function _file_path(in_file_path:string) {
 	let file_path = in_file_path
 	if (await is_readable(file_path)) {
 		return file_path
@@ -26,23 +26,23 @@ export async function resolve(
 		!specifier
 		|| specifier[0] == '.'
 		|| specifier[0] == '/'
-		|| valid__url.isUri(specifier)
+		|| url_valid.isUri(specifier)
 	) {
 		return based_on_extname(specifier)
 	}
-	const a1__NODE_PATH = _NODE_PATH_a1()
-	for (let i = 0; i < a1__NODE_PATH.length; i++) {
-		const NODE_PATH__ = a1__NODE_PATH[i]
-		const path__file =
-			await _path__file(path.join(NODE_PATH__, specifier))
-		if (path__file) {
-			return based_on_extname(path__file)
+	const NODE_PATH_a1 = _NODE_PATH_a1()
+	for (let i = 0; i < NODE_PATH_a1.length; i++) {
+		const NODE_PATH = NODE_PATH_a1[i]
+		const file_path =
+			await _file_path(path.join(NODE_PATH, specifier))
+		if (file_path) {
+			return based_on_extname(file_path)
 		}
 	}
 	return defaultResolver(specifier, parentModuleURL)
 	function based_on_extname(file_path:string) {
-		const extname__path = path.extname(file_path)
-		if (!extname__path) {
+		const path_extname = path.extname(file_path)
+		if (!path_extname) {
 			return {
 				url:
 					/^file:/.test(file_path)
@@ -50,7 +50,7 @@ export async function resolve(
 					: `file:${file_path}`,
 				format: 'cjs'
 			}
-		} else if (extname__path == '.js') {
+		} else if (path_extname == '.js') {
 			return defaultResolver(file_path, parentModuleURL)
 		} else {
 			return {
@@ -68,14 +68,14 @@ function _NODE_PATH_a1() {
 		return cache_NODE_PATH_a1
 	cache_NODE_PATH_a1 = []
 	cache_NODE_PATH = NODE_PATH
-	const a1__NODE_PATH = NODE_PATH.split(':')
-	for (let i = 0; i < a1__NODE_PATH.length; i++) {
-		let out_NODE_PATH = a1__NODE_PATH[i].trim()
-		const regexp__back = new RegExp('^\.\.')
-		const regexp__current = new RegExp('^\.')
-		if (regexp__back.test(out_NODE_PATH)) {
+	const NODE_PATH_a1 = NODE_PATH.split(':')
+	for (let i = 0; i < NODE_PATH_a1.length; i++) {
+		let out_NODE_PATH = NODE_PATH_a1[i].trim()
+		const back_regexp = new RegExp('^\.\.')
+		const current_regexp = new RegExp('^\.')
+		if (back_regexp.test(out_NODE_PATH)) {
 			out_NODE_PATH = path.join(process.cwd(), '..', out_NODE_PATH)
-		} else if (regexp__current.test(out_NODE_PATH)) {
+		} else if (current_regexp.test(out_NODE_PATH)) {
 			out_NODE_PATH = path.join(process.cwd(), out_NODE_PATH)
 		}
 		cache_NODE_PATH_a1.push(out_NODE_PATH)
