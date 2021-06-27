babfc8f1c99d4bbf608662b343c5cd47f606cffe
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 7.0.9
    ### Patch Changes
    
    - dist directory

--- src/loader-path.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function resolve(specifier: string, parentModuleURL: string, defaultResolver: (specifier: string, parentModuleURL: string) => string): Promise<string | {
-    url: string;
-    format: string;
-}>;
